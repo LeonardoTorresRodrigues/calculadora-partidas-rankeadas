2c05cba20b1c80b6a08d14430680f2720b7777ce
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2024-06-03 13:01:56 -0300

    style: add chalk

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
+const chalk = require('chalk');
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   });
   
-  console.log("Bem-vindo Herói a Calculadora de Partidas Rankeadas!");
+  console.log(chalk.blue.bold("Bem-vindo Herói, a Calculadora de Partidas Rankeadas!"));
   
   function obterEntrada(mensagem) {
     return new Promise((resolve) => {
       readline.question(mensagem, (input) => {
         if (input.toLowerCase() === 'sair') {
-          console.log("Até a próxima!");
+          console.log(chalk.yellow("Até a próxima!"));
           readline.close();
           process.exit(); // Encerra o programa completamente
         }
@@ -27,7 +28,7 @@ const readline = require('readline').createInterface({
       const losesNum = parseInt(loses, 10);
   
       if (isNaN(winsNum) || isNaN(losesNum)) {
-        console.log("Entrada inválida. Por favor, insira números válidos para vitórias e derrotas.");
+        console.log(chalk.red("Entrada inválida. Por favor, insira números válidos para vitórias e derrotas."));
         calcularRank();
         return;
       }
@@ -36,25 +37,25 @@ const readline = require('readline').createInterface({
       let saldoVitorias = winsNum - losesNum;
   
       if (saldoVitorias < 10) {
-        lvl = "Ferro";
+        lvl = chalk.gray("Ferro");
       } else if (saldoVitorias <= 20) {
-        lvl = "Bronze";
+        lvl = chalk.rgb(205, 127, 50)("Bronze");
       } else if (saldoVitorias <= 50) {
-        lvl = "Prata";
+        lvl = chalk.gray.bgWhite("Prata");
       } else if (saldoVitorias <= 80) {
-        lvl = "Ouro";
+        lvl = chalk.yellow("Ouro");
       } else if (saldoVitorias <= 90) {
-        lvl = "Diamante";
+        lvl = chalk.cyan("Diamante");
       } else if (saldoVitorias <= 100) {
-        lvl = "Lendário";
+        lvl = chalk.magenta("Lendário");
       } else {
-        lvl = "Imortal";
+        lvl = chalk.bgRed.white("Imortal");
       }
   
-      console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${lvl}`);
+      console.log(`O Herói tem um saldo de ${chalk.green(saldoVitorias)} e está no nível de ${lvl}`);
       calcularRank(); 
     } catch (error) {
-      console.error("Ocorreu um erro:", error);
+      console.error(chalk.red.bold("Ocorreu um erro:", error));
       readline.close();
     }
   }
